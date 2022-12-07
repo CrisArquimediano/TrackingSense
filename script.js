9558c297b44a0cb105c8564d51e8b464d1a5fe13
Cris Correa <artudvels@gmail.com>
2022-12-07 01:31:31 -0300

    Imlpementando obtener input de usuario y agregar fila a la tabla.

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
+var fechaInput, horaInicioInput, horaFinInput, tiempoInput, actividadInput, areaInput, estadoInput, usuarioInput
+
 document.querySelector("#btn-iniciar-act").addEventListener("click", () => {
-    addRow();
-});
+    addTask()
+})
+
+const addTask = () => {
+    getInput()
+    addRow()
+}
+
+const getInput = () => {
+
+    fechaInput = document.getElementById("fecha-input").value
+    horaInicioInput = document.getElementById("hora-inicio-input").value
+    horaFinInput = document.getElementById("hora-fin-input").value
+    tiempoInput = document.getElementById("tiempo-transcurrido").value
+    actividadInput = document.getElementById("actividad-input").value
+    areaInput = document.getElementById("area-input").value
+    estadoInput = document.getElementById("estado-input").value
+    usuarioInput = document.getElementById("usuario-input").value
+}
 
 const addRow = () => {
-    let row = document.createElement("tr");
-    let fecha = document.createElement("td");
-    let horaInicio = document.createElement("td");
-    let horaFin = document.createElement("td");
-    let tiempo = document.createElement("td");
-    let actividad = document.createElement("td");
-    let area = document.createElement("td");
-    let estado = document.createElement("td");
-    let usuario = document.createElement("td");
-
-    //hardcodeado, la idea es recibir el input del usuario para completar la fila con los datos
-    //Idea: al apretar el botón para iniciar una nueva actividad, una fila (formulario) para completar se pone en primer
-    //plano. Por detrás, cuando se termina de completar, se toma el input y se agrega finalmente la fila con este método addRow()
-    //Lo que ahora está hardcodeado será input del usuario que se debe tomar de ese form (fila que se muestra para completar)
-    //En primer lugar, realizar con éxito el tomar el input y agregar la fila, luego se verán el estilo y los efectos
-
-    addTextToColumn(fecha, `05-12-22`);
-    addTextToColumn(horaInicio, `21:00`);
-    addTextToColumn(horaFin, `-`);
-    addTextToColumn(tiempo, `2h 46'`);
-    addTextToColumn(actividad, `Desarrollo web`);
-    addTextToColumn(area, `Programación`);
-    addTextToColumn(estado, `En curso`);
-    addTextToColumn(usuario, `Cris`);
-
-
-    row.appendChild(fecha);
-    row.appendChild(horaInicio);
-    row.appendChild(horaFin);
-    row.appendChild(tiempo);
-    row.appendChild(actividad);
-    row.appendChild(area);
-    row.appendChild(estado);
-    row.appendChild(usuario);
+    let row = document.createElement("tr")
+    let fecha = document.createElement("td")
+    let horaInicio = document.createElement("td")
+    let horaFin = document.createElement("td")
+    let tiempo = document.createElement("td")
+    let actividad = document.createElement("td")
+    let area = document.createElement("td")
+    let estado = document.createElement("td")
+    let usuario = document.createElement("td")
     
-    document.querySelector("#main-table").appendChild(row);
-};
+    addTextToColumn(fecha, fechaInput)
+    addTextToColumn(horaInicio, horaInicioInput)
+    addTextToColumn(horaFin, horaFinInput)
+    addTextToColumn(tiempo, tiempoInput)
+    addTextToColumn(actividad, actividadInput)
+    addTextToColumn(area, areaInput)
+    addTextToColumn(estado, estadoInput)
+    addTextToColumn(usuario, usuarioInput)
 
-    var addTextToColumn = (column, text) => {
-        const columnText = document.createTextNode(text);
-        column.appendChild(columnText);
-    };
+
+    row.appendChild(fecha)
+    row.appendChild(horaInicio)
+    row.appendChild(horaFin)
+    row.appendChild(tiempo)
+    row.appendChild(actividad)
+    row.appendChild(area)
+    row.appendChild(estado)
+    row.appendChild(usuario)
+    
+    document.querySelector("#main-table").appendChild(row)
+}
+
+var addTextToColumn = (column, text) => {
+    const columnText = document.createTextNode(text)
+    column.appendChild(columnText)
+}
 
 
 
 //Sin implementar
-const boton = document.getElementById('btn-detener-act');
+const boton = document.getElementById('btn-detener-act')
 var msg = function() {
     alert("Detener Actividad: Aún sin implementar")
 };
-boton.addEventListener('click', msg);
+boton.addEventListener('click', msg)
 
 
